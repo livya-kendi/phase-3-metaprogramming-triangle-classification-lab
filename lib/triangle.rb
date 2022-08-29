0a057a930e108c04eec6f03d4c210ffab9309978
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-08-29 09:48:35 +0300

    commit tree 4d77c97fdf8fec67b1e22379631eac08d4dea034

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
   # write code here
+  
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def is_triangle
+    if side1 && side2 && side3 > 0
+      check1 = true
+    else check1 = false
+    end
+
+    if side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+      check2 = true
+    else check2 = false
+    end
+
+    if check1 && check2 == true
+      "Triangle"
+    else "Not triangle"
+    end
+  end
+  
+  def kind 
+    if is_triangle == "Triangle"
+      if side1 == side2 && side1 == side3
+        :equilateral
+      elsif side1 == side2 || side1 == side3 || side2 == side3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
